e5e9f41f9666cfdb56a30e21538a573069e88c8f
sakezuma <sakezuma@gmail.com>
2022-07-04 17:11:05 +0900

    jumpできるようにした

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _speed = 5f;
     [SerializeField] float _jumpPower = 5f;
+    [SerializeField] float _isGroundedLength = 1.1f;
+    [SerializeField] float _gravityPower = 0.93f;
     float h, v = 0;
     Vector3 _dir;
     Rigidbody _rb;
@@ -30,20 +32,48 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-       
+
     }
     void Move()
     {
         float speed = _dir == Vector3.zero ? 0 : _speed;
+        if (_dir == Vector3.zero)
+        {
+            // 方向の入力がニュートラルの時は、y 軸方向の速度を保持するだけ
+            _rb.velocity = new Vector3(0f, _rb.velocity.y, 0f);
+        }
+        else
+        {
+            Vector3 velo = _dir.normalized * speed;
+            velo.y = _rb.velocity.y;   // ジャンプした時の y 軸方向の速度を保持する
+            _rb.velocity = velo;   // 計算した速度ベクトルをセットする
+        }
 
-        _rb.velocity = _dir.normalized * speed;
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        Vector3 velosity = _rb.velocity;
+
+        if (IsGround())
         {
-            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+            if (Input.GetButtonDown("Jump"))
+            {
+                _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+            }
+            else if (velosity.y > 0)
+                velosity.y *= _gravityPower;
         }
+
+        _rb.velocity = velosity;
+    }
+
+    bool IsGround()
+    {
+        Vector3 start = this.transform.position;   // start: オブジェクトの Pivot
+        Vector3 end = start + Vector3.down * _isGroundedLength;  // end: start から真下の地点
+        Debug.DrawLine(start, end); // 動作確認用に Scene ウィンドウ上で線を表示する
+        bool isGrounded = Physics.Linecast(start, end); // 引いたラインに何かがぶつかっていたら true とする
+        return isGrounded;
     }
 }
