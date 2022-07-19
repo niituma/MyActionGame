361b7a640c4065867d2853c64321a9e546be7f65
SubNiitsuma <sakezuma@gmail.com>
2022-07-20 04:29:25 +0900

    ヒットストップを入れた

--- Assets/Script/TestSword.cs
+++ Assets/Script/TestSword.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TestSword : MonoBehaviour
 {
@@ -8,12 +9,28 @@ public class TestSword : MonoBehaviour
     GameObject _HitEff;
     [SerializeField]
     string _hitObjTag;
+    PlayerController _player;
+    private void Start()
+    {
+        _player = FindObjectOfType<PlayerController>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == _hitObjTag)
         {
             Vector3 hitPos = other.ClosestPointOnBounds(this.transform.position);
             Instantiate(_HitEff, hitPos, Quaternion.identity);
+            OnAttackHit();
         }
+    }
+    public void OnAttackHit()
+    {
+        var _anim = _player.GetComponent<Animator>();
+        _anim.speed = 0f;
+
+        var seq = DOTween.Sequence();
+        seq.SetDelay(0.1f);
+        // ƒ‚[ƒVƒ‡ƒ“‚ðÄŠJ
+        seq.AppendCallback(() => _anim.speed = 1f);
     }
 }
